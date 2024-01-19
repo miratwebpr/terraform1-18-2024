6700bfaaa40104f981c8b95b6172ec2792c09f0c
miratwebpr <webmiratfl@gmail.com>
2024-01-18 20:10:25 -0600

    Destroyed all infra

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0cd3c7f72edd5b06d"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "superBDSMsuper"
-  }
-}
-#fdsfasdfasdfad
--- s3.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "normis"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
-}
